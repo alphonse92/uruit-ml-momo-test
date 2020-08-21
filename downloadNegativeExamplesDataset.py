92c1030145cbd38ddadddcf3adc16738f8653ae1
Eliecer Alejandro Molina Vergel <alejandromover92@gmail.com>
2020-08-21 16:01:29 -0500

    uncomment and update notebook

--- downloadNegativeExamplesDataset.py
+++ downloadNegativeExamplesDataset.py
@@ -4,12 +4,12 @@ from lib import util
 
 util.createDatasetFolderStructure()
 
-# util.downloadImagesFromGoogle(
-#     "meme template",
-#     "meme_tmpl",
-#     100,
-#     downloadpath="dataset/raw/negative",
-# )
+util.downloadImagesFromGoogle(
+    "meme template",
+    "meme_tmpl",
+    100,
+    downloadpath="dataset/raw/negative",
+)
 
 util.downloadImagesFromGoogle(
     "person photography",
