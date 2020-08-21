229a62f7a29fd0108aa5485dc191a0a66a485df5
Eliecer Alejandro Molina Vergel <alejandromover92@gmail.com>
2020-08-21 15:22:14 -0500

    fix

--- downloadRawDataset.py
+++ downloadRawDataset.py
@@ -3,40 +3,41 @@ import sys
 from lib import util
 
 util.createDatasetFolderStructure()
-#util.createDatasetFolderStructure()(
-#     "momo",
-#     "momo_",
-#     50,
-# )
-#util.createDatasetFolderStructure()(
-#     "momo terror",
-#     "momo_terror_",
-#     50,
-# )
 
-#util.createDatasetFolderStructure()(
-#     "momo whatsapp",
-#     "momo_wsp",
-#     10,
-# )
+util.downloadImagesFromGoogle(
+    "momo",
+    "momo_",
+    50,
+)
+util.downloadImagesFromGoogle(
+    "momo terror",
+    "momo_terror_",
+    50,
+)
 
-#util.createDatasetFolderStructure()(
-#     "momo video",
-#     "momo_video",
-#     10,
-# )
+util.downloadImagesFromGoogle(
+    "momo whatsapp",
+    "momo_wsp",
+    10,
+)
 
-#util.createDatasetFolderStructure()(
-#     "momo noticias",
-#     "momo_noticias",
-#     10,
-# )
+util.downloadImagesFromGoogle(
+    "momo video",
+    "momo_video",
+    10,
+)
 
-#util.createDatasetFolderStructure()(
-#     "momo historia",
-#     "momo_historia",
-#     10,
-# )
+util.downloadImagesFromGoogle(
+    "momo noticias",
+    "momo_noticias",
+    10,
+)
+
+util.downloadImagesFromGoogle(
+    "momo historia",
+    "momo_historia",
+    10,
+)
 
 util.downloadImagesFromGoogle(
     "momo game",
