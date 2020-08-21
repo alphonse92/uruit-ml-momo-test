5d756ffc1ed98a9510d5ff40007ba5e73b0eea13
Eliecer Alejandro Molina Vergel <alejandromover92@gmail.com>
2020-08-21 15:25:00 -0500

    cleaning the dataset

---
+++ downloadNegativeExamplesDataset.py
@@ -0,0 +1,55 @@
+
+import sys
+from lib import util
+
+util.createDatasetFolderStructure()
+
+util.downloadImagesFromGoogle(
+    "momo",
+    "momo_",
+    50,
+)
+util.downloadImagesFromGoogle(
+    "momo terror",
+    "momo_terror_",
+    50,
+)
+
+util.downloadImagesFromGoogle(
+    "momo whatsapp",
+    "momo_wsp",
+    10,
+)
+
+util.downloadImagesFromGoogle(
+    "momo video",
+    "momo_video",
+    10,
+)
+
+util.downloadImagesFromGoogle(
+    "momo noticias",
+    "momo_noticias",
+    10,
+)
+
+util.downloadImagesFromGoogle(
+    "momo historia",
+    "momo_historia",
+    10,
+)
+
+util.downloadImagesFromGoogle(
+    "momo game",
+    "momo_game",
+    15,
+)
+
+util.downloadImagesFromGoogle(
+    "momo juego",
+    "momo_juego",
+    15,
+)
+
+
+
