84fcdc092f7093c31ba1871a3b540fb14be57556
Eliecer Alejandro Molina Vergel <alejandromover92@gmail.com>
2020-08-21 13:04:16 -0500

    Updating README.md

--- README.md
+++ README.md
@@ -1,5 +1,32 @@
+#  Description
+
+This repo contains the solution of the Uruit Machine Learning position (v1).
+
 # Requeriments
 
-install:
+1. Python3
+2. Selenium for python
+3. chromedriver (install for your SO)
 
-brew  cask nstall chromedriver
+# Downloading the dataset
+
+If you have mac and a default configuration just Use python to execute: `downloadRawDataset.py` .
+
+Otherwise, go to python console (or create an script to be executed) and run:
+
+```python
+
+import sys
+from lib import util
+
+# Create the folder structure for the dataset if you haven't it yet
+util.createDatasetFolderStructure()
+# Run this function to download the images
+util.downloadImagesFromGoogle(
+    "momo historia", # Search term on google images
+    "momo_historia", # Prefix of the stored images
+    10, # Max of download images
+    downloadpath="dataset/raw/", # Path where the images will be downloaded
+    chromedriver="/usr/local/bin/chromedriver" # Path of your chromedriver app 
+)
+```
--- downloadRawDataset.py
+++ downloadRawDataset.py
@@ -3,39 +3,47 @@ import sys
 from lib import util
 
 util.createDatasetFolderStructure()
-response = util.downloadImagesFromGoogle(
-    "momo",
-    "momo_",
-    50,
-)
-response = util.downloadImagesFromGoogle(
-    "momo terror",
-    "momo_terror_",
-    50,
-)
+#util.createDatasetFolderStructure()(
+#     "momo",
+#     "momo_",
+#     50,
+# )
+#util.createDatasetFolderStructure()(
+#     "momo terror",
+#     "momo_terror_",
+#     50,
+# )
 
-response = util.downloadImagesFromGoogle(
-    "momo whatsapp",
-    "momo_wsp",
-    10,
-)
+#util.createDatasetFolderStructure()(
+#     "momo whatsapp",
+#     "momo_wsp",
+#     10,
+# )
 
-response = util.downloadImagesFromGoogle(
-    "momo video",
-    "momo_video",
-    10,
-)
+#util.createDatasetFolderStructure()(
+#     "momo video",
+#     "momo_video",
+#     10,
+# )
 
-response = util.downloadImagesFromGoogle(
-    "momo noticias",
-    "momo_noticias",
-    10,
-)
+#util.createDatasetFolderStructure()(
+#     "momo noticias",
+#     "momo_noticias",
+#     10,
+# )
 
-response = util.downloadImagesFromGoogle(
+#util.createDatasetFolderStructure()(
+#     "momo historia",
+#     "momo_historia",
+#     10,
+# )
+
+util.downloadImagesFromGoogle(
     "momo historia",
     "momo_historia",
     10,
+    downloadpath="dataset/raw/",
+    chromedriver="/usr/local/bin/chromedriver"
 )
 
 
